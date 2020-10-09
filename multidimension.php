e25a01a19df377dfaa44915f9e2eab031a0aa8cc
Juan <juan.gallegomesa@gmail.com>
2020-10-08 19:45:28 -0500

    se crea tabla dinamica

---
+++ multidimension.php
@@ -0,0 +1,30 @@
+<?php 
+//Arreglo de varias dimensiones
+$productos=array(
+
+    "producto1"=>array("arroz",1600,"caribe"),
+    "producto2"=>array("aceite",5600,"premier"),
+    "producto3"=>array("sal",1000,"Refisal")
+
+);
+
+print_r($productos);
+echo("<br>");
+
+
+//recorrido
+echo("<br>");
+echo("<br>");
+foreach($productos as $valores){
+
+    echo($valores[0]);
+    echo("<br>");
+
+}
+
+
+
+
+
+
+?>
